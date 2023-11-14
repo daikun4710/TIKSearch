a3284625bf0ef7d79e344c9c968a5d2059b81762
1GiTHuB9 <a04719564@gmail.com>
2023-11-14 12:37:01 +0900

    index7 pythonから取得してピン差し

--- TikSearch/python/hashtagTest.py
+++ TikSearch/python/hashtagTest.py
@@ -10,16 +10,20 @@ ms_token = os.environ.get("ms_token","7-Xer3neNEVCuOhzJ4T6ah5XPvHJ7evMgLWT4fzw7o
 async def get_hashtag_videos():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
-        tag = api.hashtag(name="paypayドーム")
-        async for video in tag.videos(count=1,cursor=0):
+        tag = api.hashtag(name="福岡グルメ")
+        async for video in tag.videos(count=35,cursor=0):
             # print(video.as_dict)
-            print(video)
+            address=""
+            video_description = video.as_dict['desc'] 
+            if 'poi' in video.as_dict:
+                address = video.as_dict['poi']['address']
+                video_description= address+'　'+video_description
+            print(video_description) #概要　住所
+            # print(video)
             print(video.as_dict['video']['zoomCover']['960'])
             print(video.as_dict['author']['uniqueId'])
             # 住所取得 poiが存在し　郵便番号がある場合
-            # if 'poi' in video.as_dict:
-            #     address = video.as_dict['poi']['address']
-            #     print(address)
+            
 
 if __name__ == "__main__":
     asyncio.run(get_hashtag_videos())
--- TikSearch/python/hashtag_example.py
+++ TikSearch/python/hashtag_example.py
@@ -15,26 +15,22 @@ ms_token = os.environ.get("ms_token", "E7hDXPgowjq7BqwVr0wDUiiT5-FvJBa2PBbqVSglG
 async def get_hashtag_videos():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
-        tag = api.hashtag(name="福岡グルメ")
-        async for video in tag.videos(count=35, cursor=0):
-            # print("ビデオID:" + video.as_dict['id'])
-            print("ビデオ詳細" + video.as_dict['desc'])
-            # 住所取得 poiが存在し　郵便番号がある場合
-            # if 'poi' in video.as_dict:
-                # address = video.as_dict['poi']['address']
-            #     print(address.encode('utf-8').decode('utf-8') + "\n")
-            #     print(video)
-                # if '〒' in address:
-                # postal_code_index = address.index('〒')
-                # "〒"から始まる部分を含む全体の住所を取得
-                # full_address = address[postal_code_index:]
-                # print("住所情報" + full_address.encode('utf-8').decode('utf-8') + "\n")
-                # print(full_address.encode('utf-8').decode('utf-8') + "\n")
-            # いいね数 diggCount
-            # digg = video['stats']['diggCount']
-            # print(digg + address)
-            # print("ビデオ詳細" + video + "\n")
-            # break
+        tage = 0
+        result = 30
+        while tage < result:
+            tag = api.hashtag(name=args.hashtag)
+            async for video in tag.videos(count=35,cursor=tage):
+                video_description = video.as_dict['desc']
+                # 住所取得 poiが存在する場合
+                if 'poi' in video.as_dict:
+                    address = video.as_dict['poi']['address']
+                    #住所と概要を結合
+                    video_description= address+'　'+video_description
+                print(video_description) #概要　住所
+                print(video.as_dict['id']) #ID
+                print(video.as_dict['author']['uniqueId']) #ユーザー名
+                print(video.as_dict['video']['zoomCover']['960']) #サムネ
+                tage += 1
 
 if __name__ == "__main__":
     asyncio.run(get_hashtag_videos())
